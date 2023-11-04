5ab661278cdd4455213860c6f8642660a288fda8
cesar2122 <46253240+cesar2122@users.noreply.github.com>
2023-11-04 16:40:46 -0400

    Add files via upload

---
+++ js/Boleto.js
@@ -0,0 +1,45 @@
+function recarga() {
+  window.location.reload()
+}
+function compra() {
+  var nmb = document.getElementById('nombre').value;
+  var apl = document.getElementById('apellido').value;
+  var cor = document.getElementById('correo').value;
+  var pasa=0;
+  if(nmb.length == 0 || apl.length == 0 || cor.length == 0) {
+    alert('Debe colocar el nombre, apellido y correo');
+    pasa=1;
+  }
+  if (pasa==0) {
+    cant = parseInt(document.getElementById('cantidad').value);
+    tipo = document.getElementById('categoria').value;
+    console.log('cantidad ',cant);
+    console.log(tipo);
+    if ( isNaN(cant)) {
+         cant = 0;
+    }
+    valor=1;
+    console.log('cantidad convertida',cant);
+    if (cant <= 0) {
+      alert('Por favor, coloque una cantidad mayor a cero(0)');    
+    } else {
+      switch (tipo) {
+        case 'Estudiante':
+          valor=.20;
+          break;
+        case 'Trainee':
+          valor=.50;
+          break;
+        case 'Junior':
+          valor=.75;
+          break;
+          default:
+            valor=1;
+          break;
+      }
+      total=(valor*200)*cant;
+      document.getElementById('total').innerHTML ='Gracias por su compra, Total a pagar $: '+total;
+      alert('Gracias por su compra, Total a pagar $: '+total);
+    }
+  }  
+}
